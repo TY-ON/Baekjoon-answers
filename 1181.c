ea0a1cfae81133ebd93d46b4e265f42bcbc900e1
DevGyun <denuvyg11436@gmail.com>
2023-01-27 22:01:24 +0900

    1181 clear

--- 1181.c
+++ 1181.c
@@ -2,92 +2,88 @@
 #include <stdlib.h>
 #include <string.h>
 
-char** make2dArray(int rows, int columns);
-int compareLength(char* original, char* comparison);
-int comparePriority(char* original, char* comparison);
-void changeIndex(char* a, char* b);
-void freeArray(char** address);
+typedef struct _word
+{
+    char str[51];
+    int length;
+} word;
 
-int main() {
+void getWord(word* a);
+int compareStr(word o, word c);
+int checkPosition(word* arr, word c, int start);
+int insertWord(word* arr, word t, int ci);
+void printWords(word* arr, int r);
 
+int main()
+{
     int N;
-    char** words = NULL;
-    char* temp = NULL;
-
     scanf("%d", &N);
-    words = make2dArray(N, 51);
-    scanf("%s",words[0]);
-    for (int i = 1; i < N; i++)
-    {
-        scanf("%s",words[i]);
-        for (int j = i; j >= 1; j--)
-        {
-            if(comparePriority(words[j], words[j-1]) == 0)
-            {
-                changeIndex(words[j], words[j-1]);
-            }
-            else
-            {
-                break;
-            }
-        }
-    }
+    int currentIndex = 1;
+    word* words = (word*)malloc(sizeof(word) * N);
+    word tempWord;
 
-    printf("%s", words[0]);
+    getWord(&tempWord);
+    words[0] = tempWord;
     for (int i = 1; i < N; i++)
     {
-        if(comparePriority(words[i],words[i-1]) == 1)
+        getWord(&tempWord);
+        currentIndex += insertWord(words, tempWord, currentIndex);
+    }
+    printWords(words, currentIndex);
+}
+
+void getWord(word* a)
+{
+    scanf("%s", a->str);
+    a->length = strlen(a->str);
+}
+int compareStr(word o, word c)
+{
+    if (o.length > c.length)
+        return 1;
+    if (o.length < c.length)
+        return -1;
+    if (strcmp(o.str, c.str) > 0)
+        return 1;
+    if (strcmp(o.str, c.str) < 0)
+        return -1;
+    return 0;
+}
+int checkPosition(word* arr, word c, int start)
+{
+    for (int i = start; i >= 1; i--)
+    {
+        switch (compareStr(arr[i - 1], c))
         {
+        case 1:
             continue;
-        }
-        else
-        {
-            printf("%s\n", words[i]);
-        }
-    }
-    freeArray(words);
-}
 
-char** make2dArray(int m, int n)
-{
-    char** p = (char**)malloc(sizeof(char*) * m);
-    for (int i = 0; i < m; i++)
-    {
-        p[i] = (char*)malloc(sizeof(char) * n);
-    }
-    
-    return p;
-}
-int compareLength(char* o, char* c)
-{
-    if (strlen(o) > strlen(c)) return 1;
-    else if (strlen(o) == strlen(c)) return 0;
-    else return -1;
-}
-int comparePriority(char* o, char* c)
-{
-    switch(compareLength(o, c))
-    {
         case 0:
-            if(strcmp(o, c) < 0) return 0;
-            else if(strcmp(o, c) == 0) return 1;
-        case -1: return 0;
-        default:
-            return 2;
+            return -1;
+
+        case -1:
+            return start;
+        }
     }
+    return 0;
 }
-void changeIndex(char* a, char* b)
+int insertWord(word* arr, word t, int ci)
 {
-    char* temp = NULL;
-    temp = a;
-    a = b;
-    b = temp;
-}
-void freeArray(char** p)
-{
-    for(int i = 0; i < sizeof(p)/sizeof(char*); i++)
+    int position = checkPosition(arr, t, ci);
+    if (position == -1)
+        return 0;
+
+    for (int i = ci; i > position; i--)
     {
-        free(p[i]);
+        arr[i] = arr[i - 1];
     }
-    free(p);
+    arr[position] = t;
+    return 1;
+}
+void printWords(word* arr, int r)
+{
+    for (int i = 0; i < r; i++)
+    {
+        printf("%s\n", arr[i].str);
+    }
 }
