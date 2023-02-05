a0ff00eb3e94880b07f79e6f898e4dce6a736b44
TY-ON <ty05071@naver.com>
2023-02-05 12:26:31 +0900

    1929 clear

--- 1929.c
+++ 1929.c
@@ -1,59 +1,57 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<math.h>
 
-bool is_primenumber(int num, int* prime_number, int len);
+/*
+모든 테스트 케이스를 실행할 필요가 없다
+1~ 1000000까지 소수를 모두 출력하는 테스트 케이스가 없었을 것임
+--> printf로도 감당이 안되기 때문
+따라서 모두 계산하는 것보다 따로 계산하는게 더 효율적이었음
 
++ 에라토스 테네스의 체가 더 빠르다 (%보다 *가 더 빠른듯?)
 
+https://www.ibm.com/support/pages/which-better-divide-2-or-multiply-05
+So I ran a little test to see whether there's a difference. 
+Yes, indeed, there is a difference. 
+A loop with a million multiplies by 0.5 took 0.11 seconds and a loop with a million divides by 2 took 1.6 seconds. 
+So it's true for the RPG (and probably for the IBM i) that multiplying is quicker than dividing.
+*/
+
+bool is_primenumber(int num);
 
 int main()
 {
     int m, n;
     scanf("%d %d", &m, &n);
 
-    int prime_number[78498];
-    int p_index = 0;
     if(m<=2){
-        m = 3;   
-    }
-    prime_number[p_index++] = 2;
-    if(!m&1){//m%2==0 --> xor
-        m++;
+        printf("%d\n", 2);
+        m = 3;
     }
 
-    for (int i = 3; i < m; i+=2)
-    {   
-        if(is_primenumber(i, prime_number, p_index)) prime_number[p_index++] = i;
-    }
-    
+    if(!(m&1)) m++;
 
     for (int i = m; i <= n; i+=2)
-    {   
-        if(is_primenumber(i, prime_number, p_index))
-        {
-            prime_number[p_index++] = i;
-        }
-    }
-    
-    for (int i = 0; i < p_index; i++)
     {
-        printf("%d\n", prime_number[i]);
+        if(is_primenumber(i)) printf("%d\n", i);
     }
-    
 
-    scanf("%d", &n);
     return 0;
 }
 
-bool is_primenumber(int num, int* prime_number, int len)
-{
-    bool is_prime_number = true;
-    for (int i = 0; i < len; i++)
+bool is_primenumber(int num)
+{   
+    if(num == 2) return true;
+    if(!(num&1)) return false;
+
+    int square_root = sqrt(num);
+
+    for (int i = 3; i < square_root+1; i+=2)
     {
-        if(num%prime_number[i]==0)
+        if(num % i==0)
         {   
-            is_prime_number = false;
-            break;
+            return false;
         }
     }
-    return is_prime_number;
+    return true;
 }
